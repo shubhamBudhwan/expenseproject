0f36137838fad24d1b279ad39725999ee319bfe2
shubhamBudhwan <shubhambudhwanjaat@gmail.com>
2023-05-18 16:19:19 +0530

    first commit

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,11 +2,6 @@ import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 function ExpenseItem(props) {
-  ///////////////// for hard code//////////////
-  //   const expenseDate = new Date(2021, 2, 28);
-  //   const expenseTitle = "Car Insurance";
-  //   const expenseAmount = 294.7;
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
